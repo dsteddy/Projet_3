ddb6f0f1af9b19ca78d0aee749815fcabc522690
DA_SILVA Teddy <te2dydasilva@gmail.com>
2024-01-11 16:59:19 +0100

    database added

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import asyncio
-# from offres_emploi.utils import dt_to_str_iso
-# import datetime
-
+import sqlalchemy
 from scrapping import (
     create_cols_to_keep,
     fetch_all,
     job_offers_wttj,
     job_offers_pole_emploi,
 )
-
 import logging
 
 logging.basicConfig(
@@ -29,12 +26,8 @@ df_wttj = asyncio.run(fetch_all(api_links, cols_to_keep))
 # Scrapping Pole Emploi
 cols_to_drop = create_cols_to_keep('pole emploi')
 
-# start_dt = datetime.datetime(2023,9,1,12,30)
-# end_dt = datetime.datetime.today()
 params = {
     "motsCles": job_title,
-    # "minCreationDate" : dt_to_str_iso(start_dt),
-    # "maxCreationDate" : dt_to_str_iso(end_dt),
 }
 df_pole_emploi = job_offers_pole_emploi(params, cols_to_drop)
 
@@ -42,3 +35,9 @@ logging.info("Saving CSV files...")
 df_pole_emploi.to_parquet('datasets/pole_emploi_offers.parquet', index=False)
 df_wttj.to_parquet('datasets/WTTJ_offers.parquet', index=False)
 logging.info("CSV file created!")
+
+logging.info("Creating SQL Database...")
+engine = sqlalchemy.create_engine('sqlite:///database/job_offers.db')
+df_pole_emploi.to_sql('pole_emploi', con=engine, index=False, if_exists='replace')
+df_wttj.to_sql('wttj', con=engine, index=False, if_exists='replace')
+logging.info("Tables created!")
